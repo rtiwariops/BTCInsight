1214ede5ef6b18d52a1a9e8ac72144134ace42d0
Ravi Tiwari <rtiwariops@gmail.com>
2019-01-26 21:56:06 -0800

    Initial Commit

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ import os
 from app import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+app = create_app('instance/config.py')
 
 if __name__ == '__main__':
     app.run(debug=True)
