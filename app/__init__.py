f7feb7fad7f4545610abd63cb6a70a893473995e
Ravi Tiwari <rtiwariops@gmail.com>
2019-01-26 21:46:40 -0800

    Initial Commit

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 def create_app(config_name):
     app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    app.config.from_pyfile('instance/config.py')
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
     return app
